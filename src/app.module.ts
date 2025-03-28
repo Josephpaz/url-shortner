08ae73036fc0c76e77315c3786d7f1aaf5ad2ece
jspaz <josephpazjj@gmail.com>
2025-03-27 22:55:12 -0400

    feat: add entity shared core & url module;

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DrizzleModule } from './infra/drizzle/drizzle.module';
+import { UrlModule } from './modules/url/url.module';
 
 @Module({
-  imports: [DrizzleModule],
+  imports: [DrizzleModule, UrlModule],
   controllers: [AppController],
   providers: [AppService],
 })
