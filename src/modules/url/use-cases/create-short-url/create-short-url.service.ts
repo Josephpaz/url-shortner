c1f44abd60f53fbacadd2dac888cbd237706db33
jspaz <josephpazjj@gmail.com>
2025-03-30 11:26:47 -0400

    feat: save service & nanoid

--- src/modules/url/use-cases/create-short-url/create-short-url.service.ts
+++ src/modules/url/use-cases/create-short-url/create-short-url.service.ts
@@ -1,5 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { UseCase } from 'src/shared/core/use-case';
+import { CreateUrlDto } from '../../dtos/create-url.dto';
+import { Url } from '../../domain/url.entity';
+import { IUrlRepository } from '../../repositories/url-repo.interface';
+import { nanoid } from 'nanoid';
+
+type Input = CreateUrlDto;
+type Result = {
+  type: 'CreateShortUrlSuccess';
+  data: Url;
+};
 
 @Injectable()
-export class CreateShortUrlService implements UseCase<> {}
+export class CreateShortUrlService implements UseCase<Input, Result> {
+  constructor(
+    @Inject('UrlRepo')
+    private readonly urlRepository: IUrlRepository,
+  ) {}
+  async execute(input: Input): Promise<Result> {
+    const { url } = input;
+    const shortUrl = nanoid();
+
+    const urlEntity = Url.create({
+      original: url,
+      short: shortUrl,
+      clicks: 0,
+    });
+
+    const data = await this.urlRepository.save(urlEntity);
+
+    return {
+      type: 'CreateShortUrlSuccess',
+      data,
+    };
+  }
+}
